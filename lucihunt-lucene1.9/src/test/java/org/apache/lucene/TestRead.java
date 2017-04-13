7e89956e6a9dfe267ca85eeab9ac5e1a5fc464cc
lemonJun <506526593@qq.com>
2017-04-13 21:08:00 +0800

    up

---
+++ lucihunt-lucene1.9/src/test/java/org/apache/lucene/TestRead.java
@@ -0,0 +1,30 @@
+package org.apache.lucene;
+
+import java.io.IOException;
+
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.IndexInput;
+import org.junit.Test;
+
+public class TestRead {
+    @Test
+    public void readseg() {
+        try {
+            IndexInput input = FSDirectory.getDirectory("D:/testdir", false).openInput("segments");
+            System.out.println(input.readInt());
+            System.out.println(input.readLong());
+            System.out.println(input.readInt());
+            int size = input.readInt();
+            System.out.println("size" + size);
+            if (size > 0) {
+                for (int i = 0; i < size; i++) {
+                    System.out.println(input.readString());
+                    System.out.println(input.readInt());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
