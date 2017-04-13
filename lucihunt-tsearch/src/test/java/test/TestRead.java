05c1eaf2ad64998e6442be14497374e8ae556e65
lemonJun <506526593@qq.com>
2017-04-13 21:08:23 +0800

    uo

---
+++ lucihunt-tsearch/src/test/java/test/TestRead.java
@@ -0,0 +1,31 @@
+package test;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import store.FSDirectory;
+import store.IndexInput;
+
+public class TestRead {
+    @Test
+    public void readseg() {
+        try {
+            IndexInput input = FSDirectory.getDirectory("D:/testdir", false).openInput("segments");
+            System.out.println(input.readInt());
+            System.out.println(input.readLong());
+            System.out.println(input.readInt());
+            int size = input.readInt();
+            System.out.println("size" + size);
+            if (size > 0) {
+                for (int i = 0; i < size; i++) {
+                    System.out.println(input.readString());
+                    System.out.println(input.readInt());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
